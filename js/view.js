b28101bb202317ba8e5f09e47152fd1feb2e29d0
Poldus <leopold.canou@gmail.com>
2023-12-18 23:57:22 +0100

    Updated View.js

--- js/view.js
+++ js/view.js
@@ -1,24 +1,41 @@
-let V = {};
-
-V.uicalendar = new Calendar("#calendar", {
-  defaultView: "week",
-  isReadOnly: true,
-  usageStatistics: false,
-  useDetailPopup: true,
-  week: {
-    startDayOfWeek: 1,
-    dayNames: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
-    workweek: true,
-    hourStart: 8,
-    hourEnd: 20,
-    taskView: false,
-    eventView: ["time"],
+let V = {
+  type: "bar",
+  plot: {
+    styles: [
+      "#33a8c7",
+      "#52e3e1",
+      "#a0e426",
+      "#fdf148",
+      "#ffab00",
+      "#f77976",
+      "#f050ae",
+      "#d883ff",
+      "#9336fd",
+    ],
+    "border-radius": "16px 16px 0px 0px",
+    border: "2px solid #dddddd",
   },
-  template: {
-    time: function (event) {
-      return `<span style="color: white;">${event.title}</span>`;
+  "scale-x": {
+    label: {
+      /* Scale Title */ text: "Le nombre d'heure par salle",
     },
+    labels: [
+      "R01",
+      "R02",
+      "R03",
+      "R04",
+      "101",
+      "102",
+      "103",
+      "115",
+      "ADM132",
+    ] /* Scale Labels */,
   },
-});
+  series: [
+    {
+      values: [20, 45, 25, 5, 10, 15, 20, 25, 30],
+    },
+  ],
+};
 
 export { V };
--- main.js
+++ main.js
@@ -1,47 +1,9 @@
+import { V } from "./js/view.js";
 // sample events for testing
-let chartConfig = {
-  type: "bar",
-  plot: {
-    styles: [
-      "#33a8c7",
-      "#52e3e1",
-      "#a0e426",
-      "#fdf148",
-      "#ffab00",
-      "#f77976",
-      "#f050ae",
-      "#d883ff",
-      "#9336fd",
-    ],
-    "border-radius": "16px 16px 0px 0px",
-    border: "2px solid #dddddd",
-  },
-  "scale-x": {
-    label: {
-      /* Scale Title */ text: "Le nombre d'heure par salle",
-    },
-    labels: [
-      "R01",
-      "R02",
-      "R03",
-      "R04",
-      "101",
-      "102",
-      "103",
-      "115",
-      "ADM132",
-    ] /* Scale Labels */,
-  },
-  series: [
-    {
-      values: [20, 45, 25, 5, 10, 15, 20, 25, 30],
-    },
-  ],
-};
 
 zingchart.render({
   id: "myChart",
-  data: chartConfig,
+  data: V,
   height: "100%",
   width: "100%",
 });
