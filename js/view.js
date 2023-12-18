7fc9da873092d75fc296dd574724cfff12477132
Poldus <52009517+leopoldcanou@users.noreply.github.com>
2023-12-18 16:48:47 +0100

    push

--- js/view.js
+++ js/view.js
@@ -1,29 +1,24 @@
-import Calendar from '@toast-ui/calendar';
-import '@toast-ui/calendar/dist/toastui-calendar.min.css';
-
 let V = {};
 
-V.uicalendar = new Calendar('#calendar', {
-  defaultView: 'week',
+V.uicalendar = new Calendar("#calendar", {
+  defaultView: "week",
   isReadOnly: true,
   usageStatistics: false,
   useDetailPopup: true,
   week: {
     startDayOfWeek: 1,
-    dayNames: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+    dayNames: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
     workweek: true,
     hourStart: 8,
     hourEnd: 20,
     taskView: false,
-    eventView: ['time'],
+    eventView: ["time"],
   },
   template: {
-    time: function(event) {
+    time: function (event) {
       return `<span style="color: white;">${event.title}</span>`;
-    }
+    },
   },
- 
- 
 });
 
 export { V };
--- main.js
+++ main.js
@@ -1,29 +1,49 @@
 // sample events for testing
 let chartConfig = {
-  type: 'bar',
+  type: "bar",
   plot: {
-    styles: ["#33a8c7", "#52e3e1", "#a0e426", "#fdf148", "#ffab00", "#f77976", "#f050ae", "#d883ff", "#9336fd"],
-    'border-radius': "16px 16px 0px 0px",
-    'border': "2px solid #dddddd",
+    styles: [
+      "#33a8c7",
+      "#52e3e1",
+      "#a0e426",
+      "#fdf148",
+      "#ffab00",
+      "#f77976",
+      "#f050ae",
+      "#d883ff",
+      "#9336fd",
+    ],
+    "border-radius": "16px 16px 0px 0px",
+    border: "2px solid #dddddd",
   },
-  'scale-x': {
-    label: { /* Scale Title */
-      text: "Le nombre d'heure par salle",
+  "scale-x": {
+    label: {
+      /* Scale Title */ text: "Le nombre d'heure par salle",
     },
-    labels: ["R01", "R02", "R03", "R04", "101", "102", "103", "115", "ADM132"] /* Scale Labels */
+    labels: [
+      "R01",
+      "R02",
+      "R03",
+      "R04",
+      "101",
+      "102",
+      "103",
+      "115",
+      "ADM132",
+    ] /* Scale Labels */,
   },
-  series: [{
-    values: [20, 45, 25, 5, 10, 15, 20, 25, 30]
-  },
-  ]
+  series: [
+    {
+      values: [20, 45, 25, 5, 10, 15, 20, 25, 30],
+    },
+  ],
 };
 
 zingchart.render({
-  id: 'myChart',
+  id: "myChart",
   data: chartConfig,
-  height: '100%',
-  width: '100%'
+  height: "100%",
+  width: "100%",
 });
 
 // recuperate the start time from each event from the data/mmi1.ics file
-
