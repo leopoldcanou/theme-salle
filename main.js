ca5315c6002f56c4ac5439ac1441fa94552e6aae
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2023-12-19 09:54:44 +0100

    hours

--- main.js
+++ main.js
@@ -3,15 +3,6 @@ import { M } from "./js/model.js";
 
 await M.init(); // on attend que les données soient chargées
 
-// Objet qui contient les données du graphique
-let seriesObj1 = [
-  {
-    values: [20, 45, 25, 5, 10, 15, 20, 25, 30],
-  },
-];
-
-V.classcalendar.series = seriesObj1; // on ajoute les données au graphique
-
 zingchart.render({
   id: "myChart",
   data: V.classcalendar, // on appelle V.classcalendar qui est dans view.js
@@ -34,3 +25,39 @@ let durations = events.map((event) => {
 });
 console.log(durations);
 
+const locations = [
+  "R01",
+  "R02",
+  "R03",
+  "R04",
+  "101",
+  "102",
+  "103",
+  "115",
+  "ADM132",
+];
+
+const locationHours = {};
+
+durations.forEach((event) => {
+  const { duration, location } = event;
+  if (locations.includes(location)) {
+    if (locationHours[location]) {
+      locationHours[location] += duration;
+    } else {
+      locationHours[location] = duration;
+    }
+  }
+});
+
+console.log(locationHours);
+
+
+// Objet qui contient les données du graphique
+let seriesObj1 = [
+  {
+    values: [20, 45, 25, 5, 10, 15, 20, 25, 30],
+  },
+];
+
+V.classcalendar.series = seriesObj1; // on ajoute les données au graphique
