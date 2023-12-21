9ae11348470fc147ae3f80af077e0b72ac243394
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2023-12-21 09:47:46 +0100

    comit

--- main.js
+++ main.js
@@ -222,3 +222,103 @@ zingchart.render({
 });
 
 
+
+
+// iteration 2
+let classes = events.map((event) => {
+  return {
+    duration: event.duration,
+    location: event.location,
+    classType: event.classType,
+    group: event.group,
+  };
+});
+
+const locations = ["101", "102", "103", "115", "R01", "R02", "R03", "R04", "ADM132"];
+const classTypes = ["TP", "CM", "TD"];
+const groups = ["BUT1", "BUT2", "BUT3"];
+
+// get the total duration of each group (BUT1, BUT2, BUT3)
+let totalGroup = {};
+locations.forEach((location) => {
+  totalGroup[location] = {};
+  groups.forEach((group) => {
+    totalGroup[location][group] = 0;
+  });
+});
+
+classes.forEach((event) => {
+  if (locations.includes(event.location) && groups.includes(event.group)) {
+    totalGroup[event.location][event.group] += event.duration;
+  }
+});
+
+console.log(totalGroup);
+
+//get the total duration of each classType by location
+let totalClassType = {};
+classes.forEach((event) => {
+  if (locations.includes(event.location) && classTypes.includes(event.classType)) {
+    if (!totalClassType[event.location]) {
+      totalClassType[event.location] = {};
+    }
+    if (!totalClassType[event.location][event.classType]) {
+      totalClassType[event.location][event.classType] = 0;
+    }
+    totalClassType[event.location][event.classType] += event.duration;
+  }
+});
+
+console.log(totalClassType);
+
+let seriesObj1 = [];
+let seriesObj2 = [];
+
+// Loop through classTypes
+classTypes.forEach((classType) => {
+  let values = locations.map((location) => totalClassType[location][classType]);
+  seriesObj1.push({
+    values: values,
+    text: classType,
+  });
+});
+
+groups.forEach((group) => {
+  let values = locations.map((location) => totalGroup[location][group]);
+  seriesObj2.push({
+    values: values,
+    text: group,
+  });
+});
+
+V.classcalendar.series = seriesObj1;
+console.log(totalClassType);
+console.log(seriesObj2);
+V.classcalendar["scale-x"].labels = Object.keys(totalClassType);
+
+zingchart.render({
+  id: "myChart",
+  data: V.classcalendar, // on appelle V.classcalendar qui est dans view.js
+  height: "100%",
+  width: "100%",
+});
+
+
+// eventlistener on select calendartype
+
+let select = document.querySelector("#calendartype");
+
+select.addEventListener("change", () => {
+  if (select.value == "group") {
+    V.classcalendar.series = seriesObj1;
+  }
+  else if (select.value == "class") {
+    V.classcalendar.series = seriesObj2;
+  }
+  zingchart.render({
+    id: "myChart",
+    data: V.classcalendar, // on appelle V.classcalendar qui est dans view.js
+    height: "100%",
+    width: "100%",
+  });
+});
