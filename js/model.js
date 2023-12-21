cd5d4545beafe09b8ab9fba42287c383f61913fa
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2023-12-21 09:57:03 +0100

    done

--- js/model.js
+++ js/model.js
@@ -28,14 +28,3 @@ M.init = async function () {
 };
 
 export { M };
-
-/*
-    On notera que si tout ce qui est dans ce fichier concerne le modèle, seul ce qui est dans M est exporté (et donc accessible depuis l'extérieur).
-    C'est une façon de faire qui permet de garder privé les données "réelles" qui sont dans Events mais dont la visibilité est limitée à ce module/fichier.
-    Donc il faut voir M comme la partie publique de la vue et le reste comme la partie privée.
-    C'est sensiblement différent de ce qu'on faisait jusqu'à présent où tout était dans l'objet M.
-    L'utilisation des modules javascript nous permet ici de choisir ce que l'on veut rendre public ou privé.
-    C'est une autre façon d'implémenter le concept d'encapsulation sans avoir à utiliser les classes.
-    A noter qu'on aurait pu faire une classe "Model" mais dans la mesure où l'on n'aurait qu'une seule instance de Model, ce n'est pas vraiment utile.
-    
-*/
