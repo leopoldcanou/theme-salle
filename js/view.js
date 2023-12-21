af28fbead1c275ff6e46adfd4f8fcec861efd181
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2023-12-21 11:24:38 +0100

    upt

--- js/view.js
+++ js/view.js
@@ -64,4 +64,31 @@ V.classcalendar = {
   },
 };
 
+V.chartConfig = {
+  type: 'sunburst',
+  plotarea: {
+    margin: 10
+  },
+  plot: {
+    tooltip: {
+      align: 'left',
+      padding: '10px 15px',
+      borderRadius: '3px',
+      thousandsSeparator: ',',
+      text: "%t a %v heures",
+
+    },
+    valueBox: {
+      text: '%data-vbtext',
+      color: '#424242',
+      fontSize: '10px',
+      visible: null
+    },
+  },
+  options: {
+    palette: ['#33a8c7', '#52e3e1', '#a0e426', '#fdf148', '#ffab00', '#f77976', '#f050ae', '#d883ff', '#9336fd'],
+  },
+  series: [],
+};
+
 export { V };
