fc28466efb6d3a4271fdf4a1c54d6ec68d953a45
Fredeau123 <76597292+Fredeau123@users.noreply.github.com>
2023-12-21 09:55:39 +0100

    view

--- js/view.js
+++ js/view.js
@@ -17,4 +17,51 @@ V.HeatMap = {
   },
 };
 
+
+V.classcalendar = {
+  type: "bar",
+  plot: {
+    stacked: true,
+    'stack-type': "normal",
+    // styles: [
+    //   "#33a8c7",
+    //   "#52e3e1",
+    //   "#a0e426",
+    //   "#fdf148",
+    //   "#ffab00",
+    //   "#f77976",
+    //   "#f050ae",
+    //   "#d883ff",
+    //   "#9336fd",
+    // ],
+    // "border-radius": "6px",
+    // border: "1px solid #dddddd",
+    animation: {
+      "sequence": "ANIMATION_BY_NODE",
+      effect: "ANIMATION_FADE_IN",
+      speed: 500,
+      method: "ANIMATION_FADE_OUT"
+    }
+  },
+  "scale-x": {
+    label: {
+      /* Scale Title */ text: "Le nombre d'heure par salle",
+    },
+    labels: [
+
+    ] /* Scale Labels */,
+  },
+  series: [],
+
+  plotarea: {
+    margin: "dynamic"
+  },
+
+  legend: {
+    "toggle-action": "remove",
+    adjustLayout: true,
+    verticalAlign: "middle"
+  },
+};
+
 export { V };
